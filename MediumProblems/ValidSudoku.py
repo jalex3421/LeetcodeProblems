3477ea4924c285345ebcc9e36609c675a963de66
Alejandro Meza <56438011+jalex3421@users.noreply.github.com>
2022-08-22 11:58:54 +0200

    Add files via upload

---
+++ MediumProblems/ValidSudoku.py
@@ -0,0 +1,31 @@
+
+class Solution:
+    def isValidSudoku(self, board):
+        return (self.rowValidation(board) and self.columnValidation(board) and self.subgridValidation(board))
+
+    def rowValidation(self, board):
+        for row in board:
+            if self.isSubsetValid(row) != True:
+                return False
+        return True
+
+    def columnValidation(self, board):
+        for col in zip(*board):
+            if self.isSubsetValid(col) != True:
+                return False
+        return True
+
+    def subgridValidation(self, board):
+        #these are the index from the subgrids
+        for i in (0, 3, 6):
+            for j in (0, 3, 6):
+                #define the subgrid
+                subgrid = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                if self.isSubsetValid(subgrid) !=True:
+                    return False
+        return True
+
+    def isSubsetValid(self, subset):
+        subset = [i for i in subset if i != '.']
+        #return comparison between lengths
+        return (len(set(subset)) == len(subset))
