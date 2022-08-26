44270f7af0716c1e19d4f510ea4c660d24626d18
Alejandro <56438011+jalex3421@users.noreply.github.com>
2022-08-26 13:43:19 +0200

    Add files via upload

---
+++ SQL/SQL I/bigCountrys.sql
@@ -0,0 +1,5 @@
+ Write your MySQL query statement below
+
+select name as NAME, population as POPULATION, area as AREA
+from World
+where area>=3000000 or population>=25000000
---
+++ SQL/SQL I/reclableAndLowFatsProduct.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+
+select product_id as PRODUCT_ID
+from Products
+where low_fats = 'Y' and recyclable = 'Y'
