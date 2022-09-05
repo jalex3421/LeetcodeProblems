6594b48512d9affc63d58b2263cde51a65f2e977
Alejandro <56438011+jalex3421@users.noreply.github.com>
2022-09-05 10:39:46 +0200

    Delete dailyLeadsAndPartners.sql

--- EasyProblems/dailyLeadsAndPartners.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-# Table: DailySales(date_id[date],make_name[varchar],lead_id[int],partner_id[int])
-select date_id,make_name, count(distinct lead_id) as unique_leads, count(distinct partner_id) as unique_partners
-from DailySales
-group by date_id,make_name;
-
